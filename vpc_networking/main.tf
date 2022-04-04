b9d43cf2cbad9d0e578671e7d6b1e4eb634de0de
steveokay <mokaysteve@gmail.com>
2022-04-04 12:51:07 +0300

    Associate Route Table with subnets

--- vpc_networking/main.tf
+++ vpc_networking/main.tf
@@ -146,7 +146,7 @@ resource "aws_nat_gateway" "nat_gateway" {
 
 # nat gw route
 resource "aws_route" "nat_gateway_route" {
-  route_table_id = aws_route_table.public_route_table.id
+  route_table_id = aws_route_table.private_route_table.id
   nat_gateway_id = aws_nat_gateway.nat_gateway.id
   destination_cidr_block = "0.0.0.0/0"
 }
